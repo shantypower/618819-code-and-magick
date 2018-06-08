37a0fbab3472260ba91f2c3675b8fed657b1d462
shantypower <shantypower4891@yandex.ru>
2018-06-08 18:02:45 +1000

    Вывод похожих магов в окне настроек на основании шаблона

--- js/setup.js
+++ js/setup.js
@@ -1,21 +1,5 @@
 'use strict';
-// Создать функцию, возвращающую один случайный элемент массива.
-// Создать функцию, генерирующую массив из 4 объектов.
-// У каждого объекта свойства генерируются случайным образом из готовых массивов:
-//  - массив имен;
-//  - масссв фамилий;
-//( имя и фамилию склеивать в одно свойство объекта name);
-//  - массив случайных цветов мантии - поле coatColor;
-//  - массив случайных цветов глаз - для формирования поля eyesColor;
-// Создать функцию генерации DOM-элементов с помощью шаблонизации.
-//Написать функцию Вставки в эти элементы точечно контент на основании ранее созданного массива обЪектов - магов со случайными параметрами.
-//Отрисуйте сгенерированные DOM-элементы в блок .setup-similar-list. Для вставки элементов используйте DocumentFragment.
-//
-// var arr = ["Яблоко", "Апельсин", "Груша", "Лимон"];
-//var rand = Math.floor(Math.random() * arr.length);
-// alert( arr[rand] );
-
-var WIZARD_NAMES = ['Иван','Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 
@@ -44,10 +28,10 @@ var getWizardsImage = function (arrNames, arrSurnames, arrCoats, arrEyes) {
   var randomEyes = getRandomArrowElement(arrEyes);
   var wizardsImage = {};
 
-    wizardsImage.name = randomName;
-    wizardsImage.coatColor = randomCoat;
-    wizardsImage.eyesColor = randomEyes;
-    return wizardsImage
+  wizardsImage.name = randomName;
+  wizardsImage.coatColor = randomCoat;
+  wizardsImage.eyesColor = randomEyes;
+  return wizardsImage;
 };
 
 var getWizardsGroup = function (arrNames, arrSurnames, arrCoats, arrEyes) {
@@ -58,36 +42,17 @@ var getWizardsGroup = function (arrNames, arrSurnames, arrCoats, arrEyes) {
   return wizardsImages;
 };
 
-getWizardsGroup(WIZARD_NAMES, WIZARD_SURNAMES, WIZARD_COATS, WIZARD_EYES);
-/*
-var wizards = [
-  {
-    name: WIZARD_NAMES[0],
-    coatColor: 'rgb(241, 43, 107)'
-  },
-  {
-    name: WIZARD_NAMES[1],
-    coatColor: 'rgb(215, 210, 55)'
-  },
-  {
-    name: WIZARD_NAMES[2],
-    coatColor: 'rgb(101, 137, 164)'
-  },
-  {
-    name: WIZARD_NAMES[3],
-    coatColor: 'rgb(127, 127, 127)'
-  }
-];*/
+var wizards = getWizardsGroup(WIZARD_NAMES, WIZARD_SURNAMES, WIZARD_COATS, WIZARD_EYES);
 
-/*
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
-}
+};
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
@@ -95,4 +60,4 @@ for (var i = 0; i < wizards.length; i++) {
 }
 similarListElement.appendChild(fragment);
 
-userDialog.querySelector('.setup-similar').classList.remove('hidden');*/
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
